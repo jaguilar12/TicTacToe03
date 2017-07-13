6135af42c2fc12d4d67c95e0ea402d2ae48cd8bf
jaguilar12 <jaguilar@student.neumont.edu>
2017-07-13 00:18:34 -0600

    hi :)

--- app/src/main/java/com/example/jaguilar/tictactoe/GameBoard.java
+++ app/src/main/java/com/example/jaguilar/tictactoe/GameBoard.java
@@ -1,7 +1,10 @@
 package com.example.jaguilar.tictactoe;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class GameBoard extends AppCompatActivity
 {
@@ -11,5 +14,19 @@ public class GameBoard extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_board);
+        SetUpGame();
+    }
+
+    public void SetUpGame()
+    {
+        SharedPreferences sharedPreferences =
+                this.getSharedPreferences("com.example.datastorageexample",
+                        Context.MODE_PRIVATE);
+        String usernameOne = sharedPreferences.getString("one", "");
+        String usernameTwo = sharedPreferences.getString("two", "");
+        TextView test01 = (TextView)findViewById(R.id.oneName);
+        TextView test02 = (TextView)findViewById(R.id.twoName);
+        test01.setText(usernameOne);
+        test02.setText(usernameTwo);
     }
 }
--- app/src/main/java/com/example/jaguilar/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/jaguilar/tictactoe/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.jaguilar.tictactoe;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
-    //public
-    //public Spinner spin2 = (Spinner)findViewById(R.id.spinnerPlayerTwo);
+    public Spinner spin1;
+    public Spinner spin2;
+    public ArrayAdapter <String> adapter;
+    public ImageView gp1;
+    public ImageView gp2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -18,9 +27,12 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        gp1 = (ImageView)findViewById(R.id.PlayerOneGamePiece);
+        gp2 = (ImageView)findViewById(R.id.playerTwoGamePiece);
+
         String[] gamePieces = {"Circle", "Square", "Triangle", "Pentagon"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.activity_main, gamePieces);
-        Spinner spin1 = (Spinner)findViewById(R.id.spinnerPlayerOne);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, gamePieces);
+        spin1 = (Spinner)findViewById(R.id.spinnerPlayerOne);
         spin1.setAdapter(adapter);
         spin1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
         {
@@ -29,18 +41,65 @@ public class MainActivity extends AppCompatActivity
             {
                 // An item was selected. You can retrieve the selected item using
                 // parent.getItemAtPosition(pos)
+                ChooseGamePiece(i, gp1);
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView)
             {
-                  //Test
-                //testhiu
-                //tesat
+
             }
         });
+        spin2 = (Spinner)findViewById(R.id.spinnerPlayerTwo);
+        spin2.setAdapter(adapter);
+        spin2.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener()
+        {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l)
+            {
+                ChooseGamePiece(i, gp2);
+            }
 
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView)
+            {
+
+            }
+        });
     }
 
+    public void ChooseGamePiece(int i, ImageView gp)
+    {
+        switch (i)
+        {
+            case 0:
+                gp.setImageResource(R.drawable.circle);
+                break;
+            case 1:
+                gp.setImageResource(R.drawable.square);
+                break;
+            case 2:
+                gp.setImageResource(R.drawable.triangle);
+                break;
+            case 3:
+                gp.setImageResource(R.drawable.pentagon);
+                break;
+        }
+    }
 
+    public void CreateSharedPref()
+    {
+        SharedPreferences sharedPreferences =
+                this.getSharedPreferences("com.example.datastorageexample",
+                        Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("one", ((TextView)findViewById(R.id.playerOneName)).getText().toString()).apply();
+        sharedPreferences.edit().putString("two", ((TextView)findViewById(R.id.playerTwoName)).getText().toString()).apply();
+    }
+
+    public void playGameOnClick(View view)
+    {
+        CreateSharedPref();
+        Intent intent = new Intent(this, GameBoard.class);
+        startActivity(intent);
+    }
 }
